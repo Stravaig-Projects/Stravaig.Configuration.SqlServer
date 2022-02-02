dac730325b9206d3d599eca0149f98b9e681867d
Colin Mackay <github@colinmackay.scot>
2022-02-02 23:26:27 +0000

    (#10) Pick up connection string by reference

--- src/Stravaig.Configuration.SqlServer/SqlServerConfigurationBuilderExtensions.cs
+++ src/Stravaig.Configuration.SqlServer/SqlServerConfigurationBuilderExtensions.cs
@@ -9,15 +9,29 @@ public static class SqlServerConfigurationBuilderExtensions
     {
         var options = new SqlServerConfigurationOptions();
         optionsBuilder?.Invoke(options);
-
+        
+        Lazy<IConfigurationRoot> configRoot = new (builder.Build);
         if (!string.IsNullOrWhiteSpace(options.ConfigurationSection))
-            SetOptionsFromConfiguration(builder.Build(), options);
+            SetOptionsFromConfiguration(configRoot.Value, options);
+
+        // If a connection string has been supplied directly, it takes
+        // precedent over a connection string reference.
+        if (string.IsNullOrWhiteSpace(options.ConnectionString) && 
+            !string.IsNullOrWhiteSpace(options.ConnectionStringName))
+            ApplyFromConnectionStringsSection(options, configRoot.Value);
         
         return builder.Add(new SqlServerConfigurationSource(
             options.ConnectionString ?? throw new InvalidOperationException("Cannot build a SQL Server Configuration Provider with a null connection string."),
             TimeSpan.FromSeconds(options.RefreshSeconds),
             options.SchemaName,
             options.TableName));
+    }
+
+    private static void ApplyFromConnectionStringsSection(SqlServerConfigurationOptions options, IConfigurationRoot configRoot)
+    {
+        var connectionString = configRoot.GetConnectionString(options.ConnectionStringName);
+        if (!string.IsNullOrWhiteSpace(connectionString))
+            options.ConnectionString = connectionString;
     }
 
     private static void SetOptionsFromConfiguration(IConfigurationRoot configRoot, SqlServerConfigurationOptions options)
--- src/Stravaig.Configuration.SqlServer/SqlServerConfigurationOptions.cs
+++ src/Stravaig.Configuration.SqlServer/SqlServerConfigurationOptions.cs
@@ -5,6 +5,8 @@ namespace Stravaig.Configuration.SqlServer;
 public class SqlServerConfigurationOptions
 {
     public string? ConnectionString { get; set; }
+    
+    public string? ConnectionStringName { get; set; }
     public string? ConfigurationSection { get; set; }
     public string SchemaName { get; set; } = DefaultValues.SchemaName;
     public string TableName { get; set; } = DefaultValues.TableName;
