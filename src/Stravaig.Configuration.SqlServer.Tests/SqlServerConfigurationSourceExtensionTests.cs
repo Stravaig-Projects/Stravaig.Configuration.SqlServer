0f6c3bd9648072850d144fc00b78956d299e70db
Colin Mackay <github@colinmackay.scot>
2022-02-09 22:12:00 +0000

    (#16) Test warning log is correct

--- src/Stravaig.Configuration.SqlServer.Tests/SqlServerConfigurationSourceExtensionTests.cs
+++ src/Stravaig.Configuration.SqlServer.Tests/SqlServerConfigurationSourceExtensionTests.cs
@@ -1,4 +1,5 @@
 using System;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
 using Shouldly;
@@ -39,15 +40,44 @@ public class SqlServerConfigurationSourceExtensionTests
 
         var logs = captureLogger.GetLogs();
         logs.RenderLogs(Formatter.SimpleBySequence, Sink.Console);
-        
-        logs[1].OriginalMessage.ShouldBe("SQL Server Configuration Provider will retrieve from [{serverName}].[{databaseName}].[{schemaName}].[{tableName}] {frequency}. Will wait {connectionTimeout} seconds to connect, and {commandTimeout} seconds to retrieve data.");
-        logs[1].PropertyDictionary["serverName"].ShouldBe("localhost");
-        logs[1].PropertyDictionary["databaseName"].ShouldBe("testing");
-        logs[1].PropertyDictionary["schemaName"].ShouldBe("Stravaig");
-        logs[1].PropertyDictionary["tableName"].ShouldBe("AppConfiguration");
-        logs[1].PropertyDictionary["frequency"].ShouldBe("every 120 seconds");
-        logs[1].PropertyDictionary["connectionTimeout"].ShouldBe(5);
-        logs[1].PropertyDictionary["commandTimeout"].ShouldBe(10);
-    }
 
+        var descriptionLog = logs[1];
+        descriptionLog.OriginalMessage.ShouldBe("SQL Server Configuration Provider will retrieve from [{serverName}].[{databaseName}].[{schemaName}].[{tableName}] {frequency}. Will wait {connectionTimeout} seconds to connect, and {commandTimeout} seconds to retrieve data.");
+        descriptionLog.PropertyDictionary["serverName"].ShouldBe("localhost");
+        descriptionLog.PropertyDictionary["databaseName"].ShouldBe("testing");
+        descriptionLog.PropertyDictionary["schemaName"].ShouldBe("Stravaig");
+        descriptionLog.PropertyDictionary["tableName"].ShouldBe("AppConfiguration");
+        descriptionLog.PropertyDictionary["frequency"].ShouldBe("every 120 seconds");
+        descriptionLog.PropertyDictionary["connectionTimeout"].ShouldBe(5);
+        descriptionLog.PropertyDictionary["commandTimeout"].ShouldBe(10);
+    }
+    
+    [Test]
+    public void CreateLoggerWarnsOfInterleavePossibility()
+    {
+        var source = new SqlServerConfigurationSource(DummyConnectionString, expectLogger: true, refreshInterval: TimeSpan.FromSeconds(12), commandTimeout: TimeSpan.FromSeconds(30));
+        var replayLogger = (ReplayLogger<SqlServerConfigurationProvider>)source.CreateLogger();
+        var captureLogger = new TestCaptureLogger<SqlServerConfigurationProvider>();
+        replayLogger.Replay(captureLogger);
+
+        var logs = captureLogger.GetLogs();
+        logs.RenderLogs(Formatter.SimpleBySequence, Sink.Console);
+
+        var descriptionLog = logs[1];
+        descriptionLog.OriginalMessage.ShouldBe("SQL Server Configuration Provider will retrieve from [{serverName}].[{databaseName}].[{schemaName}].[{tableName}] {frequency}. Will wait {connectionTimeout} seconds to connect, and {commandTimeout} seconds to retrieve data.");
+        descriptionLog.PropertyDictionary["serverName"].ShouldBe("localhost");
+        descriptionLog.PropertyDictionary["databaseName"].ShouldBe("testing");
+        descriptionLog.PropertyDictionary["schemaName"].ShouldBe("Stravaig");
+        descriptionLog.PropertyDictionary["tableName"].ShouldBe("AppConfiguration");
+        descriptionLog.PropertyDictionary["frequency"].ShouldBe("every 12 seconds");
+        descriptionLog.PropertyDictionary["connectionTimeout"].ShouldBe(5);
+        descriptionLog.PropertyDictionary["commandTimeout"].ShouldBe(30);
+
+        var warningLog = logs[2];
+        warningLog.OriginalMessage.ShouldBe("The refresh interval, {refreshInterval} seconds, should be greater than combined connection, {connectionTimeout} seconds, and command, {commandTimeout} seconds, timeouts. A new refresh cycle may start before the previous cycle is complete.");
+        warningLog.LogLevel.ShouldBe(LogLevel.Warning);
+        warningLog.PropertyDictionary["refreshInterval"].ShouldBe(12);
+        warningLog.PropertyDictionary["connectionTimeout"].ShouldBe(5);
+        warningLog.PropertyDictionary["commandTimeout"].ShouldBe(30);
+    }
 }
